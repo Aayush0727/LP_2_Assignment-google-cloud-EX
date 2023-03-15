3805fbc257feba402256f39c3755a0d0c4f8f9fa
Aayush jain <121812866+Aayush0727@users.noreply.github.com>
2023-03-15 15:57:55 +0530

    Update binary search.cpp

--- binary search.cpp
+++ binary search.cpp
@@ -2,7 +2,35 @@
 using namespace std;
 int main()
 {
-int m;
+int m,arr[m], n;;
 cout<<"enter size of array";
 cin>>m;
-int arr
+cout<<"Enter"<<m<<"integers in assencing order";
+for(i=0,i<m,i++)
+cin>>arr[i];
+cout<<"enter numbetr you want to search";
+cin>>n;
+int start= 0, end= n-1,mid;
+  int pos= -1
+    while(start<=end) 
+    {
+      mid = (start+end)/2
+      if(arr[mid]==n){
+        pos=mid;
+        break;
+      }
+      else if(arr[mid]<n)
+      {
+        end=mid-1;
+       else
+         start=mid+1;
+      }
+      if (pos==-1)
+        cout<<n<<"is not present in array"<<endl;
+      else
+        cout<<n<<"is present in array at position;
+        return 0;
+    }
+      
+  
+ 
