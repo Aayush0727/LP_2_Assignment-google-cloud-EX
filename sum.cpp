5ae1f9d2e910f520f5eeb465f859fadfe0e338bb
Aayush jain <121812866+Aayush0727@users.noreply.github.com>
2023-03-15 15:15:12 +0530

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+   float no1,no2,sum,avg;
+   clrscr();  
+   cout<<"Enter two numbers: ";  
+   cin>>no1>>no2; 
+   sum=no1+no2;
+   avg=sum/2;
+   cout<<"sum="<<sum<<"\n";  
+   cout<<"Average="<<avg<<"\n";
+   getch(); 
+}
+
